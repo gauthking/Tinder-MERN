464e0a775f48fece5a002b4202339ef548f7b0ec
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2022-08-06 22:59:20 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyC_tDQ376mpVnQkPOx3f0bUhiaZyFbCDyc",
-    authDomain: "tinder-clone-f3767.firebaseapp.com",
-    projectId: "tinder-clone-f3767",
-    storageBucket: "tinder-clone-f3767.appspot.com",
-    messagingSenderId: "113075144558",
-    appId: "1:113075144558:web:07017e4550f6693d78237f"
+    CONFIG REMOVED DUE TO SECURITY PURPOSES
 };
 
 const app = initializeApp(firebaseConfig);
